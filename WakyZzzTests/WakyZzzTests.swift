ed0d988b7391cc214190d0464ec55f2da80782e8
Matthew Sousa <mattsousatwo@gmail.com>
2021-05-03 13:22:39 -0400

    Added tests to find future dates

--- WakyZzzTests/WakyZzzTests.swift
+++ WakyZzzTests/WakyZzzTests.swift
@@ -37,6 +37,101 @@ class WakyZzzTests: XCTestCase {
 class LocalNotificationTests: XCTestCase {
     
     let alarm = Alarm()
+    let formatter = DateFormatter()
+    let calendar = NSCalendar.current
+
+    
+    
+    func testGettingAllDates() {
+                
+//        let nextMonday = calendar.nextDate(after: Date(), matching: .init(weekday: 1), matchingPolicy: .nextTime)
+//        formatter.dateStyle = .medium
+//
+        
+        var allDates: [Date] = []
+        let today = Date()
+        let currentYear = calendar.component(.year, from: today)
+        
+        guard var nextDate = calendar.nextDate(after: today, matching: .init(weekday: 1), matchingPolicy: .nextTime) else { return }
+        let nextDateYear = calendar.component(.year, from: nextDate)
+        
+        
+        repeat {
+            let nextDateMatchingSpcecifications = nextDate
+            allDates.append(nextDateMatchingSpcecifications)
+            if let newDate = calendar.nextDate(after: nextDateMatchingSpcecifications, matching: .init(weekday: 1), matchingPolicy: .nextTime) {
+                nextDate = newDate
+            }
+        } while nextDateYear == currentYear
+    
+            
+        
+        
+        
+        print("\n\n - testGettingAllDates()" )
+        
+        for date in allDates {
+            print("\n ~ \(formatter.string(from: date))\n")
+        }
+        print(" \n - \n\n")
+        
+        
+        
+//        print("testGettingAllDates() - \n currentDate: \(formatter.string(from: Date())),\n currentDate: \(formatter.string(from: nextMonday!))")
+        
+        
+    }
+    
+    
+    func testAddingWeeksTodate() {
+        formatter.dateStyle = .medium
+        var allDates: [Date] = []
+        var monWeekday = DateComponents()
+        monWeekday.weekOfYear = 1
+        
+        
+        
+        // Compare dates years
+        func yearsMatch(_ one: Date, _ two: Date) -> Bool {
+            let yearOne = calendar.component(.year, from: one)
+            let yearTwo = calendar.component(.year, from: two)
+            switch yearOne == yearTwo {
+            case true:
+                return true
+            case false:
+                return false
+            }
+        }
+        
+        // return nextDate with component
+        func nextDate(from date: Date, component: DateComponents) -> Date? {
+            return calendar.date(byAdding: component, to: date)
+            
+        }
+        
+        
+        let today = Date()
+        
+        var newDate = Date()
+        
+        while yearsMatch(today, newDate) {
+            let lastDate = newDate
+            allDates.append(lastDate)
+            newDate = nextDate(from: newDate, component: monWeekday)!
+            
+        }
+        
+        print("\n\n - testAddingWeeksToDate()" )
+        
+        for date in allDates {
+            print("\n ~ \(formatter.string(from: date))\n")
+        }
+        print(" \n - \n\n")
+        
+        
+        
+        
+    }
     
     
 }
